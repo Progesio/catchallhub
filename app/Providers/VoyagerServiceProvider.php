296894423d37074d8de9bc8201edf0f10ad38b1d
lgarin211 <lagustinus211@gmail.com>
2025-07-03 21:03:03 +0700

    modified:   app/Providers/VoyagerServiceProvider.php

--- app/Providers/VoyagerServiceProvider.php
+++ app/Providers/VoyagerServiceProvider.php
@@ -10,9 +10,9 @@ class VoyagerServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if (class_exists('Voyager')) {
-            \Voyager::addWidget(UpdateCache::class);
-        }
+        // if (class_exists('Voyager')) {
+        //     \Voyager::addWidget(UpdateCache::class);
+        // }
     }
 
     public function register()
